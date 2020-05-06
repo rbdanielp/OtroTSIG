31c2ef6d88c121c2a71c35b7abf12c44d046d1ee
daniel <daniel@daniel-HP-Pavilion-m7-Notebook-PC>
2020-05-06 20:23:37 -0300

    asd

---
+++ Backend/src/main/java/uy/tsig/grupo16/webservices/RestBackend.java
@@ -0,0 +1,29 @@
+package uy.tsig.grupo16.webservices;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import uy.tsig.grupo16.entidades.Usuario;
+import uy.tsig.grupo16.logica.FachadaLogica;
+import uy.tsig.grupo16.logica.LogicaException;
+
+@Path("/ServiceUsuario")
+@Produces({ "application/xml", "application/json" })
+@Consumes({ "application/xml", "application/json" })
+public class RestBackend {
+
+	@POST
+	@Path("/Login/{nombre} ")
+	public Usuario login(@PathParam("nombre") String usuario,  String password) {
+		try {
+			return FachadaLogica.getUsuarioController().login(usuario, password);
+		} catch (LogicaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
